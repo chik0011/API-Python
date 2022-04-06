24aeeaaf9af56a64e99ba822ec71578b06e41839
IC-faycal <faycal@inovea-conseil.com>
2022-04-06 16:46:58 +0200

    find specific vehicule

--- main.py
+++ main.py
@@ -106,3 +106,67 @@ async def numberVehicule(request: Request):
         total = len(data_voiture)
 
     return { "TotalVehicule": total } 
+
+@app.post("/findVehicule")
+async def find_item(request: Request):
+    data = await request.json()
+
+    findVehicule = "Aucun vehicule n'a était trouvé"
+
+    with open('bateau.json') as json_data:
+            data_bateau = json.load(json_data)
+
+    for bateau in data_bateau:
+        for j in range(len(bateau)):
+            if bateau["name"] == data["name"] :
+                findVehicule = bateau
+            elif bateau["marque"] == data["marque"] :
+                findVehicule = bateau
+            elif bateau["speedMax"] == data["marque"] :
+                findVehicule = bateau
+            elif bateau["km"] == data["marque"] :
+                findVehicule = bateau
+
+    with open('avion.json') as json_data:
+            data_avion = json.load(json_data)
+
+    for avion in data_avion:
+        for j in range(len(avion)):
+            if avion["name"] == data["name"] :
+                findVehicule = avion
+            elif avion["marque"] == data["marque"] :
+                findVehicule = avion
+            elif avion["speedMax"] == data["marque"] :
+                findVehicule = avion
+            elif avion["km"] == data["marque"] :
+                findVehicule = avion
+
+    with open('moto.json') as json_data:
+            data_moto = json.load(json_data)
+
+    for moto in data_moto:
+        for j in range(len(moto)):
+            if moto["name"] == data["name"] :
+                findVehicule = moto
+            elif moto["marque"] == data["marque"] :
+                findVehicule = moto
+            elif moto["speedMax"] == data["marque"] :
+                findVehicule = moto
+            elif moto["km"] == data["marque"] :
+                findVehicule = moto
+       
+    with open('voiture.json') as json_data:
+            data_voiture = json.load(json_data)
+
+    for voiture in data_voiture:
+        for j in range(len(voiture)):
+            if voiture["name"] == data["name"] :
+                findVehicule = voiture
+            elif voiture["marque"] == data["marque"] :
+                findVehicule = voiture
+            elif voiture["speedMax"] == data["marque"] :
+                findVehicule = voiture
+            elif voiture["km"] == data["marque"] :
+                findVehicule = voiture
+       
+    return findVehicule
