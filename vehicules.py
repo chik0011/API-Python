5207847427980c9cd70f5482abc0ac96645c1f96
IC-faycal <faycal@inovea-conseil.com>
2022-04-06 15:04:49 +0200

    save element class

--- vehicules.py
+++ vehicules.py
@@ -1,38 +1,128 @@
-class Bateau:
+import _pickle as cPickle
+import traceback
+import json
+from os import path
 
+class Bateau:
     def __init__(self, name, marque, speedMax, km):
         self.name = name
         self.marque = marque
         self.speedMax = speedMax
         self.km = km
 
-"""
-class Voiture(name, marque, speedMax, km):
-    def __init__(self):
+        filename = 'bateau.json'
+        listObj = []
+
+        if path.isfile(filename) is False:
+            raise Exception("Fichier pas trouvé")
+        
+        with open(filename) as fp:
+            listObj = json.load(fp)
+        
+        listObj.append({
+            "name": self.name,
+            "marque": self.marque,
+            "speedMax": self.speedMax,
+            "km": self.km
+        })
+        
+        with open(filename, 'w') as json_file:
+            json.dump(listObj, json_file,indent=4,separators=(',',': '))
+        
+        print('Fichier modifié')
+        
+
+class Voiture:
+    def __init__(self, name, marque, speedMax, km):
         self.name = name
         self.marque = marque
         self.speedMax = speedMax
         self.km = km
 
+        filename = 'voiture.json'
+        listObj = []
 
-class Moto(name, marque, speedMax, km):
-    def __init__(self):
+        if path.isfile(filename) is False:
+            raise Exception("Fichier pas trouvé")
+        
+        with open(filename) as fp:
+            listObj = json.load(fp)
+        
+        listObj.append({
+            "name": self.name,
+            "marque": self.marque,
+            "speedMax": self.speedMax,
+            "km": self.km
+        })
+        
+        with open(filename, 'w') as json_file:
+            json.dump(listObj, json_file,indent=4,separators=(',',': '))
+        
+        print('Fichier modifié')
+
+
+class Moto:
+    def __init__(self, name, marque, speedMax, km):
         self.name = name
         self.marque = marque
         self.speedMax = speedMax
         self.km = km
 
+        filename = 'moto.json'
+        listObj = []
 
-class Avion(objetJeu, arme):
-    def __init__(self):
-        objetJeu.__init__(self)
-        arme.__init__(self)
+        if path.isfile(filename) is False:
+            raise Exception("Fichier pas trouvé")
+        
+        with open(filename) as fp:
+            listObj = json.load(fp)
+        
+        listObj.append({
+            "name": self.name,
+            "marque": self.marque,
+            "speedMax": self.speedMax,
+            "km": self.km
+        })
+        
+        with open(filename, 'w') as json_file:
+            json.dump(listObj, json_file,indent=4,separators=(',',': '))
+        
+        print('Fichier modifié')
 
-"""
+
+class Avion:
+    def __init__(self, name, marque, speedMax, km):
+        self.name = name
+        self.marque = marque
+        self.speedMax = speedMax
+        self.km = km
+
+        filename = 'avion.json'
+        listObj = []
+
+        if path.isfile(filename) is False:
+            raise Exception("Fichier pas trouvé")
+        
+        with open(filename) as fp:
+            listObj = json.load(fp)
+        
+        listObj.append({
+            "name": self.name,
+            "marque": self.marque,
+            "speedMax": self.speedMax,
+            "km": self.km
+        })
+        
+        with open(filename, 'w') as json_file:
+            json.dump(listObj, json_file,indent=4,separators=(',',': '))
+        
+        print('Fichier modifié')
+
+
 
 if __name__ == "__main__":
 
-    Bateau = Bateau("toto", "nike", 56)
+    Bateau = Bateau("toto", "nike", 56, 25)
 
     print(Bateau)
 
